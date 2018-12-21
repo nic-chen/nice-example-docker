74b9fd74db39337731fccfa0db75919b6f6fa024
chenjunxu <chenjunxu@meizu.com>
2018-12-21 21:20:10 +0800

    fix 表结构

--- mysql/init.sql
+++ mysql/init.sql
@@ -8,11 +8,12 @@ DROP TABLE IF EXISTS `test`;
 
 CREATE TABLE `member` (
   `id` bigint(20) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 -- 插入数据
-INSERT INTO `member` (`id`, `name`)
+INSERT INTO `member` (`id`, `nickname`, `avatar`)
 VALUES
-    (1,'user');
+    (1, '用户1', 'https://xxx');
